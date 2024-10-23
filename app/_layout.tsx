f37c51b8fe9b7aae386f1717911347a4f65319d1
pt7481 <Paul.tongyoo@gmail.com>
2024-10-23 15:45:48 -0700

    Interim dev on profile drawer implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,25 @@
-import { Stack } from "expo-router";
-import { View, Text, StyleSheet, Image } from "react-native";
+import { Stack, Link, router } from "expo-router";
+import { useRef } from "react";
+import { View, Text, StyleSheet, Image, Pressable } from "react-native";
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
 
 
 const styles = StyleSheet.create({
+  headerContainer: {
+    backgroundColor: '#FAF3E0',
+    height: 75
+  },
+  headerLeftContainer: {
+    position: 'absolute',
+    left: 10,
+    bottom: 5
+  },
+  headerRightContainer: {
+    position: 'absolute',
+    right: 10,
+    bottom: 5
+  },
   mainLogoContainer: {
     flexDirection: 'row',
     position: 'relative',
@@ -25,38 +41,33 @@ const styles = StyleSheet.create({
   }
 });
 
-function MainLogo() {
-  return (
-    <View style={styles.mainLogoContainer}>
-      <Text style={styles.mainLogoPart}>doo</Text>
-      <Text style={[styles.mainLogoPart, styles.secondLogoPart]}>too</Text>
-    </View>
-
-  );
-}
-
-function MainProfileIcon() {
-  return (
-    <View style={styles.mainProfileIconContainer}>
-      <Image style={styles.profileIcon} source={require('../assets/images/profile_icon_green.png')} />
-    </View>
-  );
-}
-
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Stack 
-        screenOptions={{
-          headerStyle: {
-            backgroundColor: "#FAF3E0"
-          },
-          headerTitle: '',
-          headerLeft: props => <MainLogo/>,
-          headerRight: props => <MainProfileIcon/>,
-          headerBackTitleVisible: false
-        }}
-      />
+      <Drawer 
+        screenOptions={
+        {
+          drawerPosition: 'right',
+          header: ({ navigation, route, options }) => { 
+            return (
+              <View style={styles.headerContainer}>
+                <View style={styles.headerLeftContainer}>
+                    <View style={styles.mainLogoContainer}>
+                    <Text style={styles.mainLogoPart}>doo</Text>
+                    <Text style={[styles.mainLogoPart, styles.secondLogoPart]}>too</Text>
+                  </View>
+                </View>
+                <View style={styles.headerRightContainer}>
+                  <Pressable style={styles.mainProfileIconContainer}
+                             onPress={() => navigation.openDrawer()}>
+                    <Image style={styles.profileIcon} source={require('../assets/images/profile_icon_green.png')} />
+                  </Pressable>
+                </View>
+            </View>
+            );
+          }
+        }
+      }/>
     </GestureHandlerRootView>
   );
 }
