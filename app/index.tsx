a308104c654a6939ea615321ef2cf6b4f749b1cd
pt7481 <Paul.tongyoo@gmail.com>
2024-10-24 18:51:16 -0700

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -447,7 +447,7 @@ export default function Index() {
   const showComingSoon = () => {
     Alert.alert(
       'Feature Coming Soon', // Title of the alert
-      'Stay tuned! :-)', // Message of the alert 
+      'Stay tuned!', // Message of the alert 
     );
   }
 
