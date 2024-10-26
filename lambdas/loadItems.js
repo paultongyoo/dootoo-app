a36ec5c5de52254fac28748967186422d195b13e
pt7481 <Paul.tongyoo@gmail.com>
2024-10-26 05:59:22 -0700

    Added embeddings retrieval

--- lambdas/loadItems.js
+++ lambdas/loadItems.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
@@ -6,16 +7,41 @@ export const handler = async (event) => {
         where: { anonymous_id: event.anonymous_id}
     });
     console.log(user);
-    const retreivedItems = await prisma.item.findMany({
+    const retrievedItems = await prisma.item.findMany({
         where: { 
             user: { id: user.id }
         }
     });
+
+    for (var i = 0; i < retrievedItems.length; i++) {
+      const item = retrievedItems[i];
+
+      // Obtain embedding for item text
+      console.log(`Begin retrieval and storing of embedding for item ${item.id}...`);
+      const embedding_response = await axios.post(
+          "http://ip-172-31-28-150.us-east-2.compute.internal:8000/embed",
+          { text: item.item_text }
+        );
+      const embedding = embedding_response.data.embedding;
+      console.log("Embedding: " + embedding);
+      const embeddingArray = embedding.join(',');
+
+      // Execute query to count how many similar items
+      const num_close_embeddings = await prisma.$queryRawUnsafe(
+        `SELECT COUNT(id) FROM "Item" WHERE id <> ` + item.id + ` AND 0.4 > embedding <-> '[` + 
+      embeddingArray + `]'::vector;`);
+
+      console.log("num close embeddings return: " + num_close_embeddings[0].count);
+      // append count to item object
+      retrievedItems[i].similar_count = Number(num_close_embeddings[0].count + ''); // Hack workaround to convert BigInt 
+      console.log("Updated Item: " + retrievedItems[i]);
+
+    }
   const response = {
     statusCode: 200,
-    body: JSON.stringify(retreivedItems)
+    body: JSON.stringify(retrievedItems)
   };
-  console.log("Response returned: " + retreivedItems);
+  console.log("Response returned: " + retrievedItems);
   await prisma.$disconnect();
   return response;
 };
