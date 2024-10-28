e74ef3fe6d02416b2d7406be382bcc94eef87584
pt7481 <Paul.tongyoo@gmail.com>
2024-10-28 12:19:02 -0700

    Encrypted item text on save

--- lambdas/saveItems.js
+++ lambdas/saveItems.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
+
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
+
+import AWS from 'aws-sdk';
+const kms = new AWS.KMS();
+const ITEMS_KEY_ID = process.env.ITEMS_KEY_ID;
 
 export const handler = async (event) => {
   const itemSaveCount = await saveItems(event.anonymous_id, event.items_str)
@@ -14,7 +19,7 @@ export const handler = async (event) => {
   console.log(`Returned item save count: ${itemSaveCount}`);  // TODO: itemSaveCount becomes undefined
   const response = {
     statusCode: (itemSaveCount >= 0) ? 200 : 400,
-    body: itemSaveCount,
+    body: itemSaveCount
   };
   return response;
 };
@@ -43,48 +48,64 @@ const saveItems = async(anonymous_id, items_str) => {
     for (var i = 0; i < items_arr.length; i++) {
         var array_item = items_arr[i];
 
-        const item = await prisma.item.upsert({
-            where: { task_id: array_item.task_id},
-            create: { 
-                task_id: array_item.task_id,
-                user: {
-                    connect: { id: user.id }
-                },
-                item_text: array_item.item_text,
-                is_child: array_item.is_child,
-                rank_idx: i,
-                is_done: false  // item.is_done TODO: Save done status
-            },
-            update: { 
-                task_id: array_item.task_id,
-                user: {
-                    connect: { id: user.id }
-                },
-                item_text: array_item.item_text,
-                is_child: array_item.is_child,
-                rank_idx: i,
-                is_done: false // item.is_done TODO: Save done status
-            }
-        });
-        console.log(item); 
+        // Encrypt item text before saving
+        try {
+            // Encrypt the string
+            const encryptParams = {
+              KeyId: ITEMS_KEY_ID,
+              Plaintext: Buffer.from(array_item.item_text)
+            };
+            const encryptedData = await kms.encrypt(encryptParams).promise();
+            const encryptedString = encryptedData.CiphertextBlob.toString('base64');
+            console.log("Encrypted Item Text.");
 
-        // Retrieve embedding for task and insert into table
-        // TODO: Improve this:  This currently  for tasks on every save, even those who've calculated 
-        //       embeddings in the past.
-        console.log(`Begin retrieval and storing of embedding for item ${item.id}...`);
-        const embedding_response = await axios.post(
-            "http://ip-172-31-28-150.us-east-2.compute.internal:8000/embed",
-            { text: array_item.item_text }
-          );
-        const embedding = embedding_response.data.embedding;
-        console.log("Embedding: " + embedding);
-        const embeddingArray = embedding.join(',');
-        console.log("Embedding Array: " + embeddingArray);
-        await prisma.$executeRawUnsafe(`UPDATE "Item" SET embedding = '[` + 
-            embeddingArray + `]'::vector WHERE id = ` + item.id + `;`);
-        console.log("End retreival and storage complete ..");
+            const item = await prisma.item.upsert({
+                where: { task_id: array_item.task_id},
+                create: { 
+                    task_id: array_item.task_id,
+                    user: {
+                        connect: { id: user.id }
+                    },
+                    item_text: encryptedString,
+                    is_child: array_item.is_child,
+                    rank_idx: i,
+                    is_done: false  // item.is_done TODO: Save done status
+                },
+                update: { 
+                    task_id: array_item.task_id,
+                    user: {
+                        connect: { id: user.id }
+                    },
+                    item_text: encryptedString,
+                    is_child: array_item.is_child,
+                    rank_idx: i,
+                    is_done: false // item.is_done TODO: Save done status
+                }
+            });
+            console.log(item); 
 
-        itemSaveCount += 1;
+            // Retrieve embedding for task and insert into table
+            console.log(`Begin retrieval and storing of embedding for item ${item.id}...`);
+            const embedding_response = await axios.post(
+                "http://ip-172-31-28-150.us-east-2.compute.internal:8000/embed",
+                { text: array_item.item_text }
+            );
+            const embedding = embedding_response.data.embedding;
+            console.log("Embedding: " + embedding);
+            const embeddingArray = embedding.join(',');
+            console.log("Embedding Array: " + embeddingArray);
+            await prisma.$executeRawUnsafe(`UPDATE "Item" SET embedding = '[` + 
+                embeddingArray + `]'::vector WHERE id = ` + item.id + `;`);
+            console.log("End retreival and storage complete ..");
+
+            itemSaveCount += 1;
+        } catch (error) {
+            console.error('Error encrypting or decrypting:', error);
+            return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Encryption/Decryption failed' })
+            };
+        }
     }
     return itemSaveCount;
 }
