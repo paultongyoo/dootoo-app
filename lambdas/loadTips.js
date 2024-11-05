1e9a6ab92c76edce6c1b9a2b170bd50d14794e04
pt7481 <Paul.tongyoo@gmail.com>
2024-11-05 10:19:32 -0800

    Made CTA AI lookup conditional on empty tip list

--- lambdas/loadTips.js
+++ lambdas/loadTips.js
@@ -7,6 +7,7 @@ import AWS from 'aws-sdk';
 const kms = new AWS.KMS();
 
 export const handler = async (event) => {
+    var cta = '';
     const user = await prisma.user.findUnique({
         where: { anonymous_id: event.anonymous_id}
     });
@@ -15,25 +16,6 @@ export const handler = async (event) => {
         where: { uuid: event.item_uuid }
     });
     console.log(selectedItem);
-
-    // Decrypt item text
-    const decryptParams = {
-        CiphertextBlob: Buffer.from(selectedItem.text, 'base64')
-    };
-    const decryptedData = await kms.decrypt(decryptParams).promise();
-    const decryptedString = decryptedData.Plaintext.toString('utf-8');
-
-    const completion = await openai.chat.completions.create({
-        model: "gpt-4o",
-        messages: [
-          {"role": "system", 
-            "content": "Create a short casually-worded call to action for a mobile app user to share their best tips with the community for the provided user task.  " +
-                       "Use the following format: 'Share your best tips with the community to help them <do the user task>."},
-          {"role": "user", "content": decryptedString }
-        ]
-      });
-    const cta = completion.choices[0].message.content;
-    console.log("Generated CTA: " + cta);
 
     const retrievedTips = await prisma.tip.findMany({
         where: { 
@@ -45,6 +27,29 @@ export const handler = async (event) => {
         }
     });
     console.log("Returning " + retrievedTips.length + " tips...");
+
+    if (retrievedTips.length == 0) {
+        // Decrypt item text
+        const decryptParams = {
+            CiphertextBlob: Buffer.from(selectedItem.text, 'base64')
+        };
+        const decryptedData = await kms.decrypt(decryptParams).promise();
+        const decryptedString = decryptedData.Plaintext.toString('utf-8');
+
+        const completion = await openai.chat.completions.create({
+            model: "gpt-4o",
+            messages: [
+            {"role": "system", 
+                "content": "Create a short casually-worded call to action for a mobile app user to share their best tips with the community for the provided user task.  " +
+                        "Use the following format: 'Share your best tips with the community to help them <do the user task>."},
+            {"role": "user", "content": decryptedString }
+            ]
+        });
+        cta = completion.choices[0].message.content;
+        console.log("Generated CTA: " + cta);
+    } else {
+        console.log("Skipping generating CTA since we have tips for this item.")
+    }
 
     const response = {
       statusCode: 200,
