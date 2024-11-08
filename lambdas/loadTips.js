9a49d9b94f691f1c86244e42add6cacb838feb68
pt7481 <Paul.tongyoo@gmail.com>
2024-11-08 08:52:00 -0800

    Fixed some hardcoded SQL

--- lambdas/loadTips.js
+++ lambdas/loadTips.js
@@ -39,15 +39,17 @@ export const handler = async (event) => {
     } else {
         console.log("Specified item is NOT done, returning tips of similar items (if any)...");
         retrievedTips = await prisma.$queryRawUnsafe(
-            `SELECT COALESCE(upvote_count::integer, 0) as upvote_count, "Tip".* FROM "Tip" LEFT JOIN "Item" on "Tip".item_id = "Item".id
+            `SELECT COALESCE(upvote_count::integer, 0) as upvote_count, "Tip".*
+            FROM "Tip" 
+            LEFT JOIN "Item" on "Tip".item_id = "Item".id
             LEFT JOIN (
                 SELECT "TipVote".id as "tip_id", SUM(value) as upvote_count
                 FROM "TipVote" 
                 LEFT JOIN "Tip" on "TipVote".tip_id = "Tip".id
                 LEFT JOIN "Item" on "Item".id = "Tip".item_id
-                WHERE "Tip".user_id <> 3 AND 0.7 >= embedding <-> (select embedding from "Item" where uuid = '${selectedItem.uuid}')
+                WHERE "Tip".user_id <> ${user.id} AND 0.7 >= embedding <-> (select embedding from "Item" where uuid = '${selectedItem.uuid}')
                 GROUP BY 1) tip_votes on "Tip".id = tip_votes.tip_id
-            WHERE "Tip".user_id <> 3 AND 0.7 >= embedding <-> (select embedding from "Item" where uuid = '${selectedItem.uuid}') ORDER BY 1 DESC`);
+            WHERE "Tip".user_id <> ${user.id} AND 0.7 >= embedding <-> (select embedding from "Item" where uuid = '${selectedItem.uuid}') ORDER BY 1 DESC`);
             console.log("Query returned " + retrievedTips.length + " tip(s).");
     }
 
