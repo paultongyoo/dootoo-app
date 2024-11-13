d491bfe4ac2e061a562104de994fe84cc8936eed
pt7481 <Paul.tongyoo@gmail.com>
2024-11-13 13:45:24 -0800

    Added amplitude initialization and screen tracking code

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,23 @@
-import { Slot } from 'expo-router';
+import { Slot, usePathname } from 'expo-router';
+import * as amplitude from '@amplitude/analytics-react-native';
+import { useEffect } from 'react';
+
+
+const AMPLITUDE_KEY_DEV = "28fd28b2a8714bea3efa4a0bc73fbd0b";
+const AMPLITUDE_KEY_PROD = "ac9cdda8bd0d54ba50553219f407d353";
 
 export default function Index() {
+    const pathname = usePathname();
+
+    useEffect(() => {
+        amplitude.init((__DEV__) ? AMPLITUDE_KEY_DEV : AMPLITUDE_KEY_PROD );
+    }, []);
+
+    useEffect(() => {
+        console.log("Pathname logged: " + pathname);
+        amplitude.track('Screen Viewed', {pathname: pathname});
+    }, [pathname]);
+
     return <Slot />;
 }
 
