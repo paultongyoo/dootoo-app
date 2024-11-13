5d1031dc22f38279de7b8f49eccdf28a99950792
pt7481 <Paul.tongyoo@gmail.com>
2024-11-13 09:03:30 -0800

    Added loading animation behind first launch localstorage check

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,35 @@
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { Redirect } from 'expo-router';
+import { useRouter } from 'expo-router';
 
 export default function Index() {
-  const [isFirstLaunch, setIsFirstLaunch] = useState<null | boolean>(null);
+    const router = useRouter();
 
-  useEffect(() => {
-    const checkFirstLaunch = async () => {        
-      const launchStatus = await AsyncStorage.getItem('isFirstLaunch');
-      if (launchStatus === null) {
-        setIsFirstLaunch(true);
-      } else {
-        setIsFirstLaunch(false);
-      }
-    };
-    checkFirstLaunch();
-  }, []);
+    useEffect(() => {
+        const checkFirstLaunch = async () => {
+            const launchStatus = await AsyncStorage.getItem('isFirstLaunch');
+            if (launchStatus === null) {
+               router.replace('/onboarding/step1');
+            } else {
+                router.replace('/main');
+            }
+        };
+        checkFirstLaunch();
+    }, []);
 
-  if (isFirstLaunch == null) return null;
+    const styles = StyleSheet.create({
+        container: {
+            flex: 1,
+            backgroundColor: '#DCC7AA',
+            justifyContent: 'center',
+            alignItems: 'center'
+        }
+    });
 
-  return (
-    <>
-      {isFirstLaunch ? <Redirect href="/onboarding/step1" /> : <Redirect href="/main" />}
-    </>
-  );
+    return (
+        <View style={styles.container}>
+            <ActivityIndicator size={"large"} color="black" />
+        </View>
+    );
 }
