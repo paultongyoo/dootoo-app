0ce6f0e7fdd6aa13597ae0f169bfc932882f6f39
pt7481 <Paul.tongyoo@gmail.com>
2024-11-13 02:03:21 -0800

    Added step 1 fade in animation

--- app/onboarding/step1.tsx
+++ app/onboarding/step1.tsx
@@ -1,10 +1,21 @@
 import OnboardingFooter from '@/components/OnboardingFooter';
 import { useRouter } from 'expo-router';
-import { Text, View, StyleSheet } from 'react-native'
+import { useEffect, useRef } from 'react';
+import { Text, View, StyleSheet, Animated } from 'react-native'
 import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
 export default function Step1() {
     const router = useRouter();
+    const fadeIn = useRef(new Animated.Value(0)).current;
+    const fadeInAnimation = Animated.timing(fadeIn, {
+        toValue: 1,
+        duration: 500,
+        useNativeDriver: true
+    });
+
+    useEffect(() => {
+        fadeInAnimation.start();
+    },[]);
 
     const onSwipe = ({ nativeEvent }) => {
         if (nativeEvent.state === State.END) {
@@ -20,6 +31,9 @@ export default function Step1() {
         container: {
             flex: 1,
             backgroundColor: '#DCC7AA',
+        },
+        containerToAnimate: {
+            flex: 1,
             justifyContent: 'center',
             paddingLeft: 40,
             paddingRight: 40
@@ -39,9 +53,11 @@ export default function Step1() {
     return (
         <PanGestureHandler onHandlerStateChange={onSwipe}>
             <View style={styles.container}>
-                <Text style={styles.centerCopy}>doing things{'\n'}can be <Text style={styles.red}>hard</Text>.</Text>
-                <Text style={styles.centerCopy}>doing things{'\n'}<Text style={styles.green}>together is{'\n'}easier</Text>.</Text>
-                <OnboardingFooter step={1} onForwardButtonPress={() => router.navigate('/onboarding/step2')} />
+                <Animated.View style={[styles.containerToAnimate, { opacity: fadeIn}]}>
+                    <Text style={styles.centerCopy}>doing things{'\n'}can be <Text style={styles.red}>hard</Text>.</Text>
+                    <Text style={styles.centerCopy}>doing things{'\n'}<Text style={styles.green}>together is{'\n'}easier</Text>.</Text>
+                    <OnboardingFooter step={1} onForwardButtonPress={() => router.navigate('/onboarding/step2')} />
+                </Animated.View>
             </View>
         </PanGestureHandler>
     );
