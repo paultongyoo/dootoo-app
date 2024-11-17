c3a72b835ba62bbac8bfd4a40e667294cc646197
pt7481 <Paul.tongyoo@gmail.com>
2024-11-17 03:42:02 -0800

    Moved amplitude etc code into root layout for reuse across te pages

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,19 +1,35 @@
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import React, { useEffect } from 'react';
+import { usePathname } from 'expo-router';
+import * as amplitude from '@amplitude/analytics-react-native';
+const AMPLITUDE_KEY_DEV = "28fd28b2a8714bea3efa4a0bc73fbd0b";
+const AMPLITUDE_KEY_PROD = "ac9cdda8bd0d54ba50553219f407d353";
 
 export default function StackLayout() {
+  const pathname = usePathname();
+
+  useEffect(() => {
+    amplitude.init((__DEV__) ? AMPLITUDE_KEY_DEV : AMPLITUDE_KEY_PROD);
+  }, []);
+
+  useEffect(() => {
+    //console.log("Pathname logged: " + pathname);
+    amplitude.track('Screen Viewed', { pathname: pathname });
+  }, [pathname]);
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Stack screenOptions={{
         headerShown: false
       }}>
-        <Stack.Screen name="index" options={{ headerShown: false }} />
-        <Stack.Screen name="step1" options={{ headerShown: false, animation: 'none' }} />
-        <Stack.Screen name="step2" options={{ headerShown: false }} />
-        <Stack.Screen name="step3" options={{ headerShown: false }} />
-        <Stack.Screen name="step4" options={{ headerShown: false }} />
-        <Stack.Screen name="step5" options={{ headerShown: false }} />
-        <Stack.Screen name="drawer" options={{ headerShown: false, animation: 'none'  }} />
+        <Stack.Screen name="index" />
+        <Stack.Screen name="step1" options={{ animation: 'none' }} />
+        <Stack.Screen name="step2" />
+        <Stack.Screen name="step3" />
+        <Stack.Screen name="step4" />
+        <Stack.Screen name="step5" />
+        <Stack.Screen name="drawer" options={{ animation: 'none' }} />
       </Stack>
     </GestureHandlerRootView>
   )
--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,13 @@
-import { ActivityIndicator, StyleSheet, View } from 'react-native';
-import React, { useEffect } from 'react';
+import { useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { usePathname, useRouter } from 'expo-router';
-import * as amplitude from '@amplitude/analytics-react-native';
-const AMPLITUDE_KEY_DEV = "28fd28b2a8714bea3efa4a0bc73fbd0b";
-const AMPLITUDE_KEY_PROD = "ac9cdda8bd0d54ba50553219f407d353";
+import { useRouter } from 'expo-router';
 
 export default function Index() {
-    const pathname = usePathname();
     const router = useRouter();
 
     useEffect(() => {
-        amplitude.init((__DEV__) ? AMPLITUDE_KEY_DEV : AMPLITUDE_KEY_PROD);
-
         const checkFirstLaunch = async () => {
-            console.log("Entering checkFirstLaunch");
             const launchStatus = await AsyncStorage.getItem('isFirstLaunch');
-            console.log("launchStatus: " + launchStatus);
             if (launchStatus === null) {
                 router.navigate('/step1');
             } else {
@@ -25,20 +16,15 @@ export default function Index() {
         };
         checkFirstLaunch();
     }, []);
-
-    useEffect(() => {
-        //console.log("Pathname logged: " + pathname);
-        amplitude.track('Screen Viewed', { pathname: pathname });
-    }, [pathname]);
   
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            backgroundColor: '#DCC7AA',
-            justifyContent: 'center',
-            alignItems: 'center'
-        }
-    });
+    // const styles = StyleSheet.create({
+    //     container: {
+    //         flex: 1,
+    //         backgroundColor: '#DCC7AA',
+    //         justifyContent: 'center',
+    //         alignItems: 'center'
+    //     }
+    // });
 
     return null;
     // return (
