49df0de15311e92e93c92a328bcbc5463bca1d55
pt7481 <Paul.tongyoo@gmail.com>
2024-11-16 23:43:31 -0800

    Added base wrapping background to initial layout; removed "loading your items" message from loading anim

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,7 @@
 import { Slot, usePathname } from 'expo-router';
 import * as amplitude from '@amplitude/analytics-react-native';
 import { useEffect } from 'react';
+import { View } from 'react-native';
 
 const AMPLITUDE_KEY_DEV = "28fd28b2a8714bea3efa4a0bc73fbd0b";
 const AMPLITUDE_KEY_PROD = "ac9cdda8bd0d54ba50553219f407d353";
@@ -9,14 +10,18 @@ export default function Index() {
     const pathname = usePathname();
 
     useEffect(() => {
-        amplitude.init((__DEV__) ? AMPLITUDE_KEY_DEV : AMPLITUDE_KEY_PROD );
+        amplitude.init((__DEV__) ? AMPLITUDE_KEY_DEV : AMPLITUDE_KEY_PROD);
     }, []);
 
     useEffect(() => {
         //console.log("Pathname logged: " + pathname);
-        amplitude.track('Screen Viewed', {pathname: pathname});
+        amplitude.track('Screen Viewed', { pathname: pathname });
     }, [pathname]);
 
-    return <Slot />;
+    return (
+        <View style={{ flex: 1, backgroundColor: '#DCC7AA' }}>
+            <Slot />
+        </View>
+    );
 }
 
