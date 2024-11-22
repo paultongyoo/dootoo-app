020392666550676a13b46d407d8b5790700850cb
pt7481 <Paul.tongyoo@gmail.com>
2024-11-21 20:00:19 -0800

    Added support to add link

--- components/ToastConfig.js
+++ components/ToastConfig.js
@@ -3,43 +3,57 @@ import { View, Text, Pressable } from 'react-native';
 
 const toastConfig = {
   undoableToast: ({ text1 = '', props }) => (
-    <View style={{ 
-        height: 50,
-        width: 230,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderLeftColor: '#556B2F', 
-        backgroundColor: '#FAF3E0', 
-        borderRadius: 10,
-        flexDirection: 'row', 
-        flex: 1 }}>
+    <View style={{
+      height: 50,
+      width: 230,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderLeftColor: '#556B2F',
+      backgroundColor: '#FAF3E0',
+      borderRadius: 10,
+      flexDirection: 'row',
+      flex: 1
+    }}>
       <Text style={{
-            fontSize: 16
-         }}>{text1}     </Text>
+        fontSize: 16
+      }}>{text1}     </Text>
       <Pressable onPress={props.onUndoPress}>
         <Text style={{
-            fontWeight: 'bold', 
-            color: '#A23E48', 
-            textDecorationLine: 'underline',
-            fontSize: 16}}>undo</Text>
+          fontWeight: 'bold',
+          color: '#A23E48',
+          textDecorationLine: 'underline',
+          fontSize: 16
+        }}>undo</Text>
       </Pressable>
     </View>
   ),
   msgOnlyToast: ({ text1 = '', props }) => (
-    <View style={{ 
-        height: 50,
-        width: 190,
-        padding: 15,
-        justifyContent: 'center',
-        alignItems: 'center',
-        borderLeftColor: '#556B2F', 
-        backgroundColor: '#FAF3E0', 
-        borderRadius: 10,
-        flexDirection: 'row', 
-        flex: 1 }}>
+    <View style={{
+      height: 50,
+      width: (props.width) ? props.width : 190,
+      padding: 15,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderLeftColor: '#556B2F',
+      backgroundColor: '#FAF3E0',
+      borderRadius: 10,
+      flexDirection: 'row',
+      flex: 1
+    }}>
       <Text style={{
+        fontSize: 16
+      }}>{text1}</Text>
+      {(props.linkText) ?
+        <Pressable onPress={props.onLinkPress}>
+          <Text style={{
+            fontWeight: 'bold',
+            color: '#A23E48',
+            textDecorationLine: 'underline',
             fontSize: 16
-         }}>{text1}</Text>
+          }}>{props.linkText}</Text>
+        </Pressable> : <></>
+
+      }
     </View>
   )
 };
