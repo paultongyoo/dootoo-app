0ddf93d4814690fb6550d49c423120b80a1b247a
pt7481 <Paul.tongyoo@gmail.com>
2024-11-21 20:00:35 -0800

    Made icons clickable

--- components/DootooItemSidebar.tsx
+++ components/DootooItemSidebar.tsx
@@ -1,7 +1,31 @@
-import { View, Text, Image, ActivityIndicator } from 'react-native';
+import { View, Text, Image, ActivityIndicator, Pressable } from 'react-native';
 import { formatNumber } from './Helpers';
+import Toast from 'react-native-toast-message';
+import { useContext } from 'react';
+import { AppContext } from './AppContext';
+import { useRouter } from 'expo-router';
 
 const DootooItemSidebar = ({ thing, styles }) => {
+    const router = useRouter();
+    const { setSelectedItem } = useContext(AppContext);
+    const TIPS_PATHNAME = '/meDrawer/communityDrawer/stack/tips';
+
+    const handleSimilarCountTap = (count) => {
+        Toast.show({
+            type: 'msgOnlyToast',
+            text1: `${count} ${(count > 1) ? 'people' : 'person'} had similar`,
+            position: 'bottom',
+            bottomOffset: 220,
+            props: {
+                width: 190
+            }
+        });
+    }
+
+    const handleTipCountTap = () => {
+        setSelectedItem(thing);
+        router.push(TIPS_PATHNAME);
+    }
 
     if (thing.counts_updating) {
         return (
@@ -13,15 +37,17 @@ const DootooItemSidebar = ({ thing, styles }) => {
         return (
             <> 
                 {(thing.tip_count > 0) ?
-                    <View style={styles.tipCountContainer}>
+                    <Pressable style={styles.tipCountContainer}
+                    onPress={() => handleTipCountTap()}>
                         <Text style={styles.tipCountText}>{formatNumber(thing.tip_count) || '0'}</Text>
                         <View style={styles.tipCountIcon}></View>
-                    </View> : <></>}
+                    </Pressable> : <></>}
                 {(thing.similar_count && thing.similar_count > 0) ?
-                    <View style={styles.similarCountContainer}>
+                    <Pressable style={styles.similarCountContainer}
+                               onPress={() => handleSimilarCountTap(thing.similar_count)}>
                         <Text style={styles.similarCountText}>{formatNumber(thing.similar_count)}</Text>
                         <Image style={styles.similarCountIcon} source={require("@/assets/images/person_icon_556B2F.png")} />
-                    </View> : <></>}
+                    </Pressable> : <></>}
             </>
         );
     }
