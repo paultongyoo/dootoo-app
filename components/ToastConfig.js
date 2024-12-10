37f4e645d87affdaedba3569d9f3a63228d943cf
pt7481 <Paul.tongyoo@gmail.com>
2024-12-09 16:00:24 -0800

    Refactored styles definitions in ToastConifg

--- components/ToastConfig.js
+++ components/ToastConfig.js
@@ -1,76 +1,65 @@
 import React from 'react';
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
 import { momentFromNow } from './Helpers';
+
+
+const styles = StyleSheet.create({
+  baseToast: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderLeftColor: '#556B2F',
+    backgroundColor: '#FAF3E0',
+    borderRadius: 10,
+    flexDirection: 'row',
+    flex: 1
+  },
+  undoableToast: { 
+    height: 50, 
+    width: 230 
+  },
+  msgWithLink: {
+    height: 50,
+    padding: 15
+  },
+  timerInfo: {
+    padding: 15
+  },
+  toastText: {
+    fontSize: 16
+  },
+  toastLink: {
+    fontWeight: 'bold',
+    color: '#A23E48',
+    textDecorationLine: 'underline',
+    fontSize: 16
+  }
+});
 
 const toastConfig = {
   undoableToast: ({ text1 = '', props }) => (
-    <View style={{
-      height: 50,
-      width: 230,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderLeftColor: '#556B2F',
-      backgroundColor: '#FAF3E0',
-      borderRadius: 10,
-      flexDirection: 'row',
-      flex: 1
-    }}>
-      <Text style={{
-        fontSize: 16
-      }}>{text1}     </Text>
+    <View style={[styles.baseToast, styles.undoableToast]}>
+      <Text style={styles.toastText}>{text1}     </Text>
       <Pressable onPress={props.onUndoPress}>
-        <Text style={{
-          fontWeight: 'bold',
-          color: '#A23E48',
-          textDecorationLine: 'underline',
-          fontSize: 16
-        }}>undo</Text>
+        <Text style={styles.toastLink}>undo</Text>
       </Pressable>
     </View>
   ),
   msgWithLink: ({ text1 = '', props }) => (
-    <View style={{
-      height: 50,
-      width: (props.width) ? props.width : 190,
-      padding: 15,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderLeftColor: '#556B2F',
-      backgroundColor: '#FAF3E0',
-      borderRadius: 10,
-      flexDirection: 'row',
-      flex: 1
-    }}>
-      <Text style={{
-        fontSize: 16
-      }}>{text1}</Text>
+    <View style={[
+      styles.baseToast, 
+      styles.msgWithLink,
+      { width: (props.width) ? props.width : 190 }]}>
+      <Text style={styles.toastText}>{text1}</Text>
       {(props.linkText) ?
         <Pressable onPress={props.onLinkPress}>
-          <Text style={{
-            fontWeight: 'bold',
-            color: '#A23E48',
-            textDecorationLine: 'underline',
-            fontSize: 16
-          }}>{props.linkText}</Text>
+          <Text style={styles.toastLink}>{props.linkText}</Text>
         </Pressable> : <></>
-
       }
     </View>
   ),
   timerInfo: ({ props }) => (
-    <View style={{
-      padding: 15,
-      justifyContent: 'center',
-      alignItems: 'center',
-      borderLeftColor: '#556B2F',
-      backgroundColor: '#FAF3E0',
-      borderRadius: 10,
-      flexDirection: 'row',
-      flex: 1
-    }}>
-      <Text style={{
-        fontSize: 16
-      }}>{momentFromNow(props.thing)}</Text>
+    <View style={[styles.baseToast, styles.timerInfo]}>
+      <Text style={styles.toastText}>{momentFromNow(props.thing)}</Text>
     </View>
   )
 };
