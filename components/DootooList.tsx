11c6c2f88c9020811c6886c48d1c207ebd9f5660
pt7481 <Paul.tongyoo@gmail.com>
2024-12-15 11:28:39 -0800

    Added potential infinite loop fix

--- components/DootooList.tsx
+++ components/DootooList.tsx
@@ -1159,7 +1159,7 @@ const DootooList = ({ thingName = THINGNAME_ITEM, loadingAnimMsg = null, listArr
             //console.log("renderItem useEFfect([item.text]) - item.text: " + item.text);
             if (isInitialTextMount.current) {
                 isInitialTextMount.current = false;
-            } else if ((thingName == THINGNAME_ITEM) && item.text && (item.text.length > 0)) {
+            } else if ((thingName == THINGNAME_ITEM) && item.text && (item.text.length > 0) && !item.schedule_just_added) {
                 const attemptToEnrichedItem = async (itemToEnrich) => {
                     try {
                         const enrichmentResponse = await fetchWithRetry(() => enrichItem(itemToEnrich));
@@ -1184,6 +1184,7 @@ const DootooList = ({ thingName = THINGNAME_ITEM, loadingAnimMsg = null, listArr
                                     : thing
                                     ));
                             
+                            // TODO: Move these two operations to the enrichment lambda
                             const deepItemCopy = { ... item,
                                                     text: enrichmentResponse.text,
                                                     scheduled_datetime_utc: enrichmentResponse.scheduled_datetime_utc }
