f3201b53dc51b26f955aeb05491283d2c59b0147
pt7481 <Paul.tongyoo@gmail.com>
2024-12-17 18:41:58 -0800

    Removed obsolete comment

--- components/svg/arrow-left.tsx
+++ components/svg/arrow-left.tsx
@@ -1,7 +1,6 @@
 import Svg, { Path } from "react-native-svg";
 
 // Source: https://lucide.dev/icons/arrow-left
-// Chagning stroke-width appears to have no effect
 export const ArrowLeft = ({ wxh, color, strokeWidth }) => (
     <Svg width={wxh} height={wxh} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} stroke-linecap="round" stroke-linejoin="round">
         <Path d="m12 19-7-7 7-7" />
