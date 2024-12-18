3706f2c80912fd0d6818be1155602e09e82eebb2
pt7481 <Paul.tongyoo@gmail.com>
2024-12-17 19:49:46 -0800

    Corrected attributes on chevron left SVG

--- components/svg/chevron-left.tsx
+++ components/svg/chevron-left.tsx
@@ -2,7 +2,7 @@ import Svg, { Path } from "react-native-svg";
 
 // https://lucide.dev/icons/chevron-left
 export const ChevronLeft = ({wxh, color, strokeWidth}) => (
-    <Svg width={wxh} height={wxh} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-left">
+    <Svg width={wxh} height={wxh} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
         <Path d="m15 18-6-6 6-6"/>
     </Svg>
 )
