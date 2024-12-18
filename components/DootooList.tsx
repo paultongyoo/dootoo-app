104c6f326fad3ea9359b4a009e47d056564e9d3a
pt7481 <Paul.tongyoo@gmail.com>
2024-12-18 13:52:35 -0800

    Commented out render level log message

--- components/DootooList.tsx
+++ components/DootooList.tsx
@@ -1132,7 +1132,7 @@ const DootooList = ({ thingName = THINGNAME_ITEM, loadingAnimMsg = null, listArr
             // If row had a height of 0 on render, assume it was just collapsed via a prior animation
             // and restore it to full height.  See special case for first reveal of list in the isFinished clause
             if (rowHeight.value == 0) {
-                console.log("Expanding row " + getIndex() + " to full height, firstListRendered.value: " + firstListRendered.value);
+                //console.log("Expanding row " + getIndex() + " to full height, firstListRendered.value: " + firstListRendered.value);
                 rowHeight.value = withTiming(fullRowHeight.current, { duration: 300 }, (isFinished) => {
                     if (isFinished) {
 
