7a4a92926f6adea538cf0923920f8d053abe054f
pt7481 <Paul.tongyoo@gmail.com>
2024-12-17 19:50:40 -0800

    Corrected attributes on arrow left svg

--- components/svg/arrow-left.tsx
+++ components/svg/arrow-left.tsx
@@ -2,7 +2,10 @@ import Svg, { Path } from "react-native-svg";
 
 // https://lucide.dev/icons/arrow-left
 export const ArrowLeft = ({ wxh, color, strokeWidth }) => (
-    <Svg width={wxh} height={wxh} viewBox="0 0 24 24" fill="none" stroke={color} strokeWidth={strokeWidth} stroke-linecap="round" stroke-linejoin="round">
+    <Svg width={wxh} height={wxh} 
+         viewBox="0 0 24 24" fill="none" 
+         stroke={color} strokeWidth={strokeWidth} 
+         strokeLinecap="round" strokeLinejoin="round">
         <Path d="m12 19-7-7 7-7" />
         <Path d="M19 12H5" />
     </Svg>
