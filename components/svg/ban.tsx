93324d8030632b39f1571315988077f9409a506e
pt7481 <Paul.tongyoo@gmail.com>
2024-12-17 19:09:51 -0800

    Corrected ban SVG to remove fill

--- components/svg/ban.tsx
+++ components/svg/ban.tsx
@@ -1,9 +1,13 @@
 import Svg, { Circle, Path } from "react-native-svg";
 
 // Source: https://lucide.dev/icons/ban
-export const Ban = ({ wxh, color, strokeWidth, opacity = "1.0" }) => (
-    <Svg width={wxh} height={wxh} viewBox="0 0 24 24" fill="none" opacity={opacity} stroke={color} strokeWidth={strokeWidth} strokeLinecap="round" strokeLinejoin="round">
-        <Circle cx="12" cy="12" r="10" />
-        <Path d="m4.9 4.9 14.2 14.2" />
+export const Ban = ({ wxh, color, strokeWidth = "3", opacity = "1.0" }) => (
+    <Svg width={wxh} height={wxh} viewBox="0 0 24 24" 
+         opacity={opacity} stroke={color} 
+         strokeWidth={strokeWidth} 
+         strokeLinecap="round" 
+         strokeLinejoin="round" >
+        <Circle fill="none" cx="12" cy="12" r="10"/>
+        <Path d="m4.9 4.9 14.2 14.2"/>
     </Svg>
 )
