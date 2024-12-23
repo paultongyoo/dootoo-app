eb6f7c120b7d054d28ee81f877f680b454f9ed55
pt7481 <Paul.tongyoo@gmail.com>
2024-12-22 17:22:06 -0800

    Moved OpenAI API check to tabs layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,58 @@
+import { checkOpenAPIStatus } from '@/components/BackendServices';
 import DootooFooter from '@/components/DootooFooter';
 import DootooHeader from '@/components/DootooHeader';
 import { Tabs } from 'expo-router';
+import { useEffect } from 'react';
+import { Alert, AppState } from 'react-native';
+import * as amplitude from '@amplitude/analytics-react-native';
 
 export default function TabLayout() {
+
+  useEffect(() => {
+
+    // Initialize App State event handlers
+    const handleAppStateChange = (nextAppState) => {
+      if (nextAppState === "active") {
+        checkOpenAPIHealth();
+      }
+    };
+    const subscription = AppState.addEventListener("change", handleAppStateChange);
+    checkOpenAPIHealth();
+
+    return () => {
+      subscription.remove();
+    }
+  }, [])
+
+
+  const checkOpenAPIHealth = async () => {
+    const status = await checkOpenAPIStatus();
+    console.log("OpenAPI Health Status: " + status);
+    if (status != "operational") {
+      amplitude.track("OpenAI API Impacted Prompt Displayed");
+      Alert.alert(
+        "AI Features May Be Impacted",
+        "Please be aware that our AI partner is currently experiencing issues that may impact new voice recordings and text edits.  " +
+        "This message will cease to appear once their issues are resolved.",
+        [
+          {
+            text: 'OK',
+            onPress: () => {
+              amplitude.track("OpenAI API Impacted Prompt OK'd");
+            },
+          },
+        ],
+        { cancelable: true } // Optional: if the alert should be dismissible by tapping outside of it
+      );
+    }
+  }
+
+
   return (
     <Tabs
       tabBar={(props) => (<DootooFooter {...props} />)}
       screenOptions={{
-        header: (props) => (<DootooHeader {...props} />)
+        header: (props) => (<DootooHeader {...props} />),
       }}>
       <Tabs.Screen name="list" />
       <Tabs.Screen name="done" />
