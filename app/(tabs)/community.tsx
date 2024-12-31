a737c2f145bd369100151def4ded1446eb959863
pt7481 <Paul.tongyoo@gmail.com>
2024-12-30 23:01:40 -0800

    Initial impl of community loading anim

--- app/(tabs)/community.tsx
+++ app/(tabs)/community.tsx
@@ -1,7 +1,43 @@
-import { Text } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, View, ActivityIndicator } from "react-native";
+import Animated, { useSharedValue, withTiming } from "react-native-reanimated";
 
 const CommunityScreen = () => {
-    return <Text>Community Screen!</Text>
+
+    const [screenInitialized, setScreenInitialized] = useState(false);
+    const initialLoadFadeInOpacity = useSharedValue(0);
+
+    useEffect(() => {
+        console.log("Community.useEffect([])");
+        initialLoadFadeInOpacity.value = withTiming(1, { duration: 300});
+        return () => {
+            console.log("Community.useEffect([]) cleanup");
+            initialLoadFadeInOpacity.value = withTiming(0, { duration: 300}); 
+        }
+    }, []);
+    
+
+    const styles = StyleSheet.create({
+        container: {
+            backgroundColor: '#DCC7AA',
+            flex: 1
+        },
+        initialLoadAnimContainer: {
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center'
+        },
+    })
+    return (
+        <View style={styles.container}>
+            {(!screenInitialized) ?
+                <Animated.View style={[styles.initialLoadAnimContainer, { opacity: initialLoadFadeInOpacity }]}>
+                    <ActivityIndicator size={"large"} color="#3E3723" />
+                </Animated.View>
+                : <></>
+            }
+        </View>
+    )
 }
 
 export default CommunityScreen;
