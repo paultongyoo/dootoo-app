90ca3089d40c8ec08cab6f53b6f5750339d55e5e
pt7481 <Paul.tongyoo@gmail.com>
2024-12-30 21:55:51 -0800

    Removed unused imports

--- components/DootooItemSidebar.tsx
+++ components/DootooItemSidebar.tsx
@@ -1,34 +1,18 @@
-import { View, Text, Platform, ActivityIndicator, Pressable, StyleSheet, Alert } from 'react-native';
-import { formatNumber } from './Helpers';
-import Toast from 'react-native-toast-message';
-import { useContext, useEffect, useState } from 'react';
+import { View, ActivityIndicator, Pressable, StyleSheet, Alert } from 'react-native';
+import { useContext, useState } from 'react';
 import { AppContext } from './AppContext';
-import { usePathname, useRouter } from 'expo-router';
-import { LIST_ITEM_EVENT__POLL_ITEM_COUNTS_RESPONSE, ListItemEventEmitter } from "@/components/EventEmitters";
-import Animated, { runOnJS, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
-import { flushTipsCache, updateItemPublicState } from './Storage';
+import { usePathname } from 'expo-router';
+import Animated, { useSharedValue } from 'react-native-reanimated';
+import { updateItemPublicState } from './Storage';
 import * as amplitude from '@amplitude/analytics-react-native';
-import { Bulb } from './svg/bulb';
-import { UserRound } from './svg/user-round';
 import { UsersRound } from './svg/users-round';
 
 
-const DootooItemSidebar = ({ thing, disabled = false }) => {
-    const router = useRouter();
+const DootooItemSidebar = ({ thing }) => {
     const pathname = usePathname();
-    const { anonymousId, setSelectedItem, itemCountsMap } = useContext(AppContext);
-
-    const TIPS_PATHNAME = '/(tabs)/tips';
-
-    const [tipCount, setTipCount] =
-        useState((itemCountsMap.current && itemCountsMap.current.get(thing.uuid)) ? itemCountsMap.current.get(thing.uuid).tip_count : null);
-    const [similarCount, setSimilarCount] =
-        useState((itemCountsMap.current && itemCountsMap.current.get(thing.uuid)) ? itemCountsMap.current.get(thing.uuid).similar_count : null);
+    const { anonymousId } = useContext(AppContext);
 
     const [isPublic, setIsPublic] = useState(thing.is_public == true);
-
-    // Update v1.1.1:  Experimenting with not displaying loading anim on item level now that race condition is solved
-    const [loading, setLoading] = useState(false);
 
     // const opacitySV = useSharedValue(1);
     // const opacityAnimatedStyle = useAnimatedStyle(() => {
@@ -159,32 +143,23 @@ const DootooItemSidebar = ({ thing, disabled = false }) => {
         }
     })
 
-    // Update v1.1.1:  Experimenting with not displaying loading anim on item level now that race condition is solved
-    if (loading) {
+    const greenColorSV = useSharedValue("#556B2F")
+
+    if (!thing.parent_item_uuid) {
         return (
-            <View style={sidebarStyles.itemCountsRefreshingAnimContainer}>
-                <ActivityIndicator size={"small"} color="#3E3723" />
-            </View>
+            <Animated.View style={[{ flexDirection: 'row' }]}>
+                <Pressable hitSlop={{ top: 10, bottom: 10, left: 10 }}
+                    style={sidebarStyles.isPublicContainer}
+                    onPress={() => handleIsPublicTap()}>
+                    <UsersRound
+                        wxh="20"
+                        opacity={(isPublic) ? "1.0" : "0.3"}
+                        color={greenColorSV} />
+                </Pressable>
+            </Animated.View>
         );
     } else {
-        const greenColorSV = useSharedValue("#556B2F")
-
-        if (!thing.parent_item_uuid) {
-            return (
-                <Animated.View style={[/*opacityAnimatedStyle,*/ { flexDirection: 'row' }]}>
-                    <Pressable hitSlop={{ top: 10, bottom: 10, left: 10 }}
-                        style={sidebarStyles.isPublicContainer}
-                        onPress={() => handleIsPublicTap()}>
-                        <UsersRound
-                            wxh="20"
-                            opacity={(isPublic) ? "1.0" : "0.3"}
-                            color={greenColorSV} />
-                    </Pressable>
-                </Animated.View>
-            );
-        } else {
-            return <></>
-        }
+        return <></>
     }
 };
 
