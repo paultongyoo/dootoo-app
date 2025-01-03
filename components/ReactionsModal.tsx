7fe3342efb14e396c99d3d42a2a5e3e0677e77d1
pt7481 <Paul.tongyoo@gmail.com>
2025-01-02 19:48:09 -0800

    First draft animation of reactions modal header

--- components/ReactionsModal.tsx
+++ components/ReactionsModal.tsx
@@ -1,4 +1,4 @@
-import { View, StyleSheet, Pressable, Text, FlatList } from "react-native";
+import { View, StyleSheet, Pressable, Text, FlatList, Alert } from "react-native";
 import Modal from "react-native-modal";
 import { ThumbUp } from "./svg/thumb-up";
 import { Heart } from "./svg/heart";
@@ -6,8 +6,9 @@ import { Laugh } from "./svg/laugh";
 import { HandHeart } from "./svg/hand-heart";
 import { PartyPopper } from "./svg/party-popper";
 import * as Constants from './Constants';
+import Animated, { Easing, runOnJS, useSharedValue, withTiming } from "react-native-reanimated";
 
-const ReactionsModal = ({modalVisible, modalVisibleSetter, reactions, reactionCounts}) => {
+const ReactionsModal = ({ modalVisible, modalVisibleSetter, reactions, reactionCounts }) => {
 
     const styles = StyleSheet.create({
         reactorsModal: {
@@ -18,15 +19,55 @@ const ReactionsModal = ({modalVisible, modalVisibleSetter, reactions, reactionCo
             borderTopRightRadius: 10,
             width: '100%',
             height: '50%',
-            padding: 10
+            padding: 20
         },
         reactorsModalHeader: {
-
+            borderBottomWidth: 1,
+            borderBottomColor: "#3E272333",
+            marginBottom: 15
+        },
+        reactorsModalHeaderSections: {
+            flexDirection: 'row'
         },
         reactorsModalHeaderSection: {
-
+            flexDirection: 'row',
+            paddingBottom: 10,
+            paddingRight: 5,
+            marginRight: 15
+        },
+        reactorsModalHeaderSectionText: {
+            fontWeight: 'bold',
+            fontSize: 16
+        },
+        reactorsModalHeaderSectionCount: {
+            paddingLeft: 8
+        },
+        currentSectionIndicator: {    
+            marginRight: 18,  // Keep these synced with sectionIcon padding
+            width: 38,
+            height: 4,
+            backgroundColor: "#556b2f"
         }
-    })
+    });
+
+    const updateReactionsFilter = (idx) => {
+        //Alert.alert("Update reactions filter to idx: " + idx);
+    }
+
+    const barTranslateX = useSharedValue(0);
+    const animateCurrentSectionIndicator = (reactionIndex) => {
+        // 0 = First section
+        // 1 = Second section
+        // 2 = Third Section
+        barTranslateX.value = withTiming((reactionIndex + 1) * 56, {
+            duration: 150,
+            easing: Easing.out(Easing.exp)
+        }, (isFinished) => {
+            if (isFinished) {
+                runOnJS(updateReactionsFilter)(reactionIndex);
+            }
+        });
+    }
 
     return (
         <Modal
@@ -39,43 +80,49 @@ const ReactionsModal = ({modalVisible, modalVisibleSetter, reactions, reactionCo
             animationOut={"slideOutDown"}>
             <View style={styles.reactorsModal}>
                 <View style={styles.reactorsModalHeader}>
-                    { (Object.keys(reactionCounts).length > 1) 
+                    <View style={styles.reactorsModalHeaderSections}>
+                        {(Object.keys(reactionCounts).length > 1)
                             ? <>
-                                <Pressable style={styles.reactorsModalHeaderSection}>
-                                    <Text>All</Text>
+                                <Pressable onPress={() => animateCurrentSectionIndicator(-1)}
+                                            style={styles.reactorsModalHeaderSection}>
+                                    <Text style={styles.reactorsModalHeaderSectionText}>All</Text>
+                                    <Text style={[styles.reactorsModalHeaderSectionText, styles.reactorsModalHeaderSectionCount]}>{reactions.length}</Text>
                                 </Pressable>
-                                {Object.keys(reactionCounts).map(reaction  => (
+                                {Object.keys(reactionCounts).map((reaction, index) => (
                                     <View key={reaction}>
-                                        <Pressable style={styles.reactorsModalHeaderSection}>
+                                        <Pressable  onPress={() => animateCurrentSectionIndicator(index)}
+                                                    style={styles.reactorsModalHeaderSection}>
                                             {(reaction == Constants.REACTION_LIKE) ? <ThumbUp wxh="20" color="#556B2F" />
                                                 : (reaction == Constants.REACTION_LOVE) ? <Heart wxh="20" color="#556B2F" />
                                                     : (reaction == Constants.REACTION_LAUGH) ? <Laugh wxh="20" color="#556B2F" />
                                                         : (reaction == Constants.REACTION_SUPPORT) ? <HandHeart wxh="20" color="#556B2F" />
                                                             : <PartyPopper wxh="20" color="#556B2F" />
                                             }
-                                        </Pressable>
-                                        <Text>{reactionCounts[reaction as string]}</Text>
+                                            <Text style={[styles.reactorsModalHeaderSectionText, styles.reactorsModalHeaderSectionCount]}>{reactionCounts[reaction as string]}</Text>
+                                        </Pressable>                                    
                                     </View>
                                 ))}
-                              </>
-                            :                                     <>
-                            <Pressable style={styles.reactorsModalHeaderSection}>
-                                {(Object.keys(reactionCounts)[0] == Constants.REACTION_LIKE) ? <ThumbUp wxh="20" color="#556B2F" />
-                                    : (Object.keys(reactionCounts)[0] == Constants.REACTION_LOVE) ? <Heart wxh="20" color="#556B2F" />
-                                        : (Object.keys(reactionCounts)[0] == Constants.REACTION_LAUGH) ? <Laugh wxh="20" color="#556B2F" />
-                                            : (Object.keys(reactionCounts)[0] == Constants.REACTION_SUPPORT) ? <HandHeart wxh="20" color="#556B2F" />
-                                                : <PartyPopper wxh="20" color="#556B2F" />
-                                }
-                            </Pressable>
-                            <Text>{reactionCounts[Object.keys(reactionCounts)[0]] as string}</Text>
-                        </>
-                    }
+                            </>
+                            : <>
+                                <View style={styles.reactorsModalHeaderSection}>
+                                    {(Object.keys(reactionCounts)[0] == Constants.REACTION_LIKE) ? <ThumbUp wxh="20" color="#556B2F" />
+                                        : (Object.keys(reactionCounts)[0] == Constants.REACTION_LOVE) ? <Heart wxh="20" color="#556B2F" />
+                                            : (Object.keys(reactionCounts)[0] == Constants.REACTION_LAUGH) ? <Laugh wxh="20" color="#556B2F" />
+                                                : (Object.keys(reactionCounts)[0] == Constants.REACTION_SUPPORT) ? <HandHeart wxh="20" color="#556B2F" />
+                                                    : <PartyPopper wxh="20" color="#556B2F" />
+                                    }
+                                </View>
+                                <Text style={[styles.reactorsModalHeaderSectionText, styles.reactorsModalHeaderSectionCount]}>{reactionCounts[Object.keys(reactionCounts)[0]] as string}</Text>
+                            </>
+                        }
+                    </View>
+                    <Animated.View style={[styles.currentSectionIndicator, { transform: [{ translateX: barTranslateX }] }]}></Animated.View>
                 </View>
                 <FlatList data={reactions}
-                        renderItem={({ item, index, separators }) =>
-                            <Text>{item.reaction.name}: {item.user.name}</Text>
-                        }
-                        keyExtractor={(item, index) => `${item.user.name}_${item.reaction.name}`} />
+                    renderItem={({ item, index, separators }) =>
+                        <Text>{item.reaction.name}: {item.user.name}</Text>
+                    }
+                    keyExtractor={(item, index) => `${item.user.name}_${item.reaction.name}`} />
             </View>
         </Modal>
     )
