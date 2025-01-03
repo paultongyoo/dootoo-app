2f1d41d189e260d16f869476eb984256f5acf253
pt7481 <Paul.tongyoo@gmail.com>
2025-01-03 11:01:49 -0800

    Fixed delete query per updated schema

--- lambdas/reactToItem.js
+++ lambdas/reactToItem.js
@@ -40,10 +40,9 @@ export const handler = async (event) => {
     if (event.delete) {
       const deletedReaction = await prisma.userReaction.delete({
         where: {
-          user_id_item_id_reaction_id: {
+          user_id_item_id: {
             user_id: user.id,
-            item_id: item.id,
-            reaction_id: reaction.id
+            item_id: item.id
           }
         }
       });
