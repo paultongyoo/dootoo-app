3058a184fadb39e564b2874df130ff21a7ee08cc
pt7481 <Paul.tongyoo@gmail.com>
2025-01-07 20:25:19 -0800

    Split mic step into two steps for now

--- components/DootooFirstLaunchUX.tsx
+++ components/DootooFirstLaunchUX.tsx
@@ -79,25 +79,13 @@ const DootooFirstLaunchUX = () => {
     setCurrentStep(4);
   }
 
-  const speakNaturallyOpacity = useSharedValue(0);
-  const speakNaturallyAnimatedStyle = useAnimatedStyle(() => {
-    return { opacity: speakNaturallyOpacity.value }
-  })
   const executeStep4Animation = async () => {
-    await new Promise<void>((resolve) => 
-      coachmarkOpacity.value = withTiming(1, { duration: 500 }, (isFinished) => {
+    await new Promise<void>((resolve) =>
+      coachmarkOpacity.value = withTiming(1, { duration: 1000 }, (isFinished) => {
         if (isFinished) {
-          runOnJS(resolve)();
+          runOnJS(resolve)()
         }
-    }));
-
-    await new Promise<void>((resolve) => 
-      speakNaturallyOpacity.value = withDelay(1000, withTiming(1, { duration: 1000 }, (isFinished) => {
-        if (isFinished) {
-          runOnJS(resolve)();
-        }
-    })));
-
+      }));
     amplitude.track("Onboarding Step 4 Viewed");
     await new Promise<void>((resolve) => stepOpacity.value = withDelay(1000, withTiming(0, { duration: 1000 }, (isFinished) => {
       if (isFinished) {
@@ -108,13 +96,37 @@ const DootooFirstLaunchUX = () => {
   }
 
   const executeStep5Animation = async () => {
-    coachmarkTranslateX.value = withTiming(BOTTOM_BUTTON_WIDTH_INCL_MARGINS / 2, 
-      { 
-        duration: 500,
-        easing: Easing.out(Easing.exp)
-      }
-    )
     amplitude.track("Onboarding Step 5 Viewed");
+    await new Promise<void>((resolve) => stepOpacity.value = withDelay(1000, withTiming(0, { duration: 1000 }, (isFinished) => {
+      if (isFinished) {
+        runOnJS(resolve)();
+      }
+    })));
+    setCurrentStep(6);
+  }
+
+  const executeStep6Animation = async () => {
+    const animationPromises = [
+      new Promise<void>((resolve) =>
+        stepOpacity.value = withTiming(1, { duration: 1000 }, (isFinished) => {
+          if (isFinished) {
+            runOnJS(resolve)()
+          }
+        })),
+      new Promise<void>((resolve) =>
+        coachmarkTranslateX.value = withTiming(BOTTOM_BUTTON_WIDTH_INCL_MARGINS / 2,
+          {
+            duration: 500,
+            easing: Easing.out(Easing.exp)
+          },
+          (isFinished) => {
+            if (isFinished) {
+              runOnJS(resolve)();
+            }
+          }))
+    ];
+    await Promise.all(animationPromises);
+    amplitude.track("Onboarding Step 6 Viewed");
     endFirstLaunchUX();
   }
 
@@ -133,7 +145,7 @@ const DootooFirstLaunchUX = () => {
 
     isFirstLaunch.current = false;
     await AsyncStorage.setItem('isFirstLaunch', 'false');
-    NavigationEventEmitter.emit(NAVIGATION_EVENT__GO_TO_SECTION, NAVIGATION_SECTION_IDX_OPEN); 
+    NavigationEventEmitter.emit(NAVIGATION_EVENT__GO_TO_SECTION, NAVIGATION_SECTION_IDX_OPEN);
   }
 
 
@@ -171,6 +183,8 @@ const DootooFirstLaunchUX = () => {
           runOnJS(executeStep5Animation)();
         }
       });
+    } else if (currentStep == 6 && !skipped.current) {
+      executeStep6Animation();
     }
   }, [currentStep])
 
@@ -234,9 +248,9 @@ const DootooFirstLaunchUX = () => {
 
   return <Animated.View style={[styles.emptyListContainer, { opacity }]}>
     <View style={styles.skipButtonContainer}>
-      <Pressable hitSlop={10} 
-                 style={({pressed}) => pressed && { backgroundColor: '#3e272310' }} 
-                 onPress={handleSkipTap}>
+      <Pressable hitSlop={10}
+        style={({ pressed }) => pressed && { backgroundColor: '#3e272310' }}
+        onPress={handleSkipTap}>
         <Text style={styles.skipButtonText}>skip</Text>
       </Pressable>
     </View>
@@ -252,41 +266,43 @@ const DootooFirstLaunchUX = () => {
       {(currentStep == 2) && (
         <>
           <Text style={styles.centerCopy}>doo<Text style={styles.red}>too</Text> helps you
-          <Text style={styles.green}> easily capture your most important things to do</Text>...</Text>
+            <Text style={styles.green}> easily capture your most important things to do</Text>...</Text>
         </>
       )}
       {(currentStep == 3) && (
         <>
-          <Text style={styles.centerCopy}>...and 
-          <Text style={styles.green}> connects you with a community of doo-ers </Text>
-          like you.</Text>
+          <Text style={styles.centerCopy}>...and
+            <Text style={styles.green}> connects you with a community of doo-ers </Text>
+            like you.</Text>
         </>
       )}
       {(currentStep == 4) && (
         <>
-          <Text style={styles.centerCopy}>tap the 
-          <Text style={styles.green}> microphone to quickly add many things at once</Text>
-          .</Text>
-          <Animated.View style={[{position: 'relative', top: -10 }, speakNaturallyAnimatedStyle]}>
-            <Text style={styles.centerCopy}><Text style={styles.green}>speak naturally </Text>
-            and 
-            <Text style={styles.green}> just let your thoughts flow</Text>
+          <Text style={styles.centerCopy}>tap the
+            <Text style={styles.green}> microphone to quickly add many things at once</Text>
             .</Text>
-          </Animated.View>
         </>
       )}
       {(currentStep == 5) && (
         <>
-          <Text style={styles.centerCopy}>tap the
-          <Text style={styles.green}> keyboard to add one thing at a time</Text>
-          .</Text>
+          <Text style={styles.centerCopy}><Text style={styles.green}>speak naturally </Text>
+            and
+            <Text style={styles.green}> just let your thoughts flow</Text>
+            .</Text>
+        </>
+      )}
+      {(currentStep == 6) && (
+        <>
+          <Text style={styles.centerCopy}>Tap the
+            <Text style={styles.green}> keyboard to add one thing at a time</Text>
+            .</Text>
         </>
       )}
     </Animated.View>
     <Animated.View style={[styles.coachmarksContainer, coachmarkAnimatedOpacity]}>
       <Animated.View style={[styles.downArrowContainer, { transform: [{ translateX: coachmarkTranslateX }] }]}>
         <ArrowDown wxh="50" color="#556B2F" />
-       </Animated.View>
+      </Animated.View>
     </Animated.View>
   </Animated.View>;
 };
