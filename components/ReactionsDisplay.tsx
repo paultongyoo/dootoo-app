86abe6b9a7663deb14412c346a87c4af57e23b72
pt7481 <Paul.tongyoo@gmail.com>
2025-01-08 19:50:14 -0800

    Added pressed state on reactions display component

--- components/ReactionsDisplay.tsx
+++ components/ReactionsDisplay.tsx
@@ -7,11 +7,12 @@ import { PartyPopper } from "./svg/party-popper"
 import * as Constants from "@/components/Constants";
 
 
-export const ReactionsDisplay = ({reactions, onReactionsPress}) => {
+export const ReactionsDisplay = ({ reactions, onReactionsPress }) => {
     const styles = StyleSheet.create({
         reactions: {
             flexDirection: 'row',
-            alignItems: 'center'
+            alignItems: 'center',
+            paddingHorizontal: 4
         },
         reaction: {
             paddingRight: 5
@@ -27,15 +28,18 @@ export const ReactionsDisplay = ({reactions, onReactionsPress}) => {
     return (
         <>
             {(reactions.length > 0) ?
-                <Pressable style={styles.reactions} onPress={onReactionsPress}>
-                    {[...new Set(reactions.map((ur) => ur.reaction.name))].map(reaction_name => (
-                        (reaction_name == Constants.REACTION_LIKE) ? <View key={reaction_name} style={styles.reaction}><ThumbUp wxh="20" color="#556B2F" /></View>
-                            : (reaction_name == Constants.REACTION_LOVE) ? <View key={reaction_name} style={styles.reaction}><Heart wxh="20" color="#556B2F" /></View>
-                                : (reaction_name == Constants.REACTION_LAUGH) ? <View key={reaction_name} style={styles.reaction}><Laugh wxh="20" color="#556B2F" /></View>
-                                    : (reaction_name == Constants.REACTION_SUPPORT) ? <View key={reaction_name} style={styles.reaction}><HandHeart wxh="20" color="#556B2F" /></View>
-                                        : <View key={reaction_name} style={styles.reaction}><PartyPopper wxh="20" color="#556B2F" /></View>
-                    ))}
-                    <Text style={styles.reactionCount}>{reactions.length}</Text>
+                <Pressable style={({ pressed }) => [{alignSelf: 'flex-start'}, pressed && { backgroundColor: '#3e272310' }]}
+                    onPress={onReactionsPress}>
+                    <View style={styles.reactions}>
+                        {[...new Set(reactions.map((ur) => ur.reaction.name))].map(reaction_name => (
+                            (reaction_name == Constants.REACTION_LIKE) ? <View key={reaction_name} style={styles.reaction}><ThumbUp wxh="20" color="#556B2F" /></View>
+                                : (reaction_name == Constants.REACTION_LOVE) ? <View key={reaction_name} style={styles.reaction}><Heart wxh="20" color="#556B2F" /></View>
+                                    : (reaction_name == Constants.REACTION_LAUGH) ? <View key={reaction_name} style={styles.reaction}><Laugh wxh="20" color="#556B2F" /></View>
+                                        : (reaction_name == Constants.REACTION_SUPPORT) ? <View key={reaction_name} style={styles.reaction}><HandHeart wxh="20" color="#556B2F" /></View>
+                                            : <View key={reaction_name} style={styles.reaction}><PartyPopper wxh="20" color="#556B2F" /></View>
+                        ))}
+                        <Text style={styles.reactionCount}>{reactions.length}</Text>
+                    </View>
                 </Pressable>
                 : <></>}
         </>
