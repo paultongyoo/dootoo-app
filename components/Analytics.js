37a0518b34f5c3fb8264788f55fc66ea6e0e591e
pt7481 <Paul.tongyoo@gmail.com>
2025-01-21 10:57:14 -0800

    Added app version to clickstream props

--- components/Analytics.js
+++ components/Analytics.js
@@ -2,9 +2,14 @@ import * as amplitude from '@amplitude/analytics-react-native';
 import { trackClickstream } from './Storage';
 
 export const trackEvent = (eventName, eventProperties) => {
-    console.log("Tracking: " + eventName + ", properties: " + JSON.stringify(eventProperties));
+    const appVersion = '1.7.2';
+    const clickStreamProps = { 
+        app_version: appVersion, 
+        ...eventProperties
+    };
+    console.log("Tracking: " + eventName + ", appendedProps: " + JSON.stringify(clickStreamProps));
     if (!__DEV__) {
-        trackClickstream(eventName, eventProperties);
+        trackClickstream(eventName, clickStreamProps);
     }
     amplitude.track(eventName, eventProperties);
   }
